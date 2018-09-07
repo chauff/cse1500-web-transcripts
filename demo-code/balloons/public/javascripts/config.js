4677a8b63944248f43d3fe291bd645220039a926
Claudia <claudia@wlan-145-94-191-46.wlan.tudelft.nl>
2018-09-07 13:30:17 +0200

    port changed

--- demo-code/balloons/public/javascripts/config.js
+++ demo-code/balloons/public/javascripts/config.js
@@ -5,7 +5,7 @@
     exports.MAX_ALLOWED_GUESSES = 7;                /* Maximum number of guesses; TODO: based on it, set number of balloons */
     exports.MIN_WORD_LENGTH = 5;                    /* Minimum allowed word length */
     exports.MAX_WORD_LENGTH = 15;                   /* Maximum allowed word length */
-    exports.WEB_SOCKET_URL = "ws://localhost:3001"; /* WebSocket URL */
+    exports.WEB_SOCKET_URL = "ws://localhost:3000"; /* WebSocket URL */
     exports.HIDDEN_CHAR = "#";                      /* Hidden char of the UI */
 
 }(typeof exports === "undefined" ? this.Setup = {} : exports));
