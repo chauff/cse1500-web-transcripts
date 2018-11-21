4346f1de30002e7a3b07008ae1b1296ce5c95810
Claudia Hauff <claudia.hauff@gmail.com>
2018-11-21 16:36:02 +0100

    url routing code fixed

--- demo-code/node-url-routing-ex/web.js
+++ demo-code/node-url-routing-ex/web.js
@@ -1,12 +1,35 @@
 var http = require("http");
+var url = require("url");
 
 var port = process.argv[2];
 
-var server = http.createServer(function (req, res) {
-    res.writeHead(200, { "Content-Type": "text/plain" });
-    res.end("Hello World!");
-    console.log("HTTP response sent");
-})
+function simpleHTTPResponder(req, res) {
+
+    //parse the URL
+    var uParts = url.parse(req.url, true);
+
+    //implemented path
+    if (uParts.pathname == "/greetme"){
+        res.writeHead(200, { "Content-Type": "text/plain" });
+
+        //parse the query
+        var query = uParts.query;
+        var name = "Anonymous";
+
+        if (query["name"] != undefined){
+            name = query["name"];
+        }
+
+        res.end(" Greetings "+name);
+    }
+    //all other paths
+    else {
+        res.writeHead(404, { "Content-Type": "text/plain" });
+        res.end("Only /greetme is implemented.");
+    }
+}
+
+var server = http.createServer(simpleHTTPResponder);
 
 server.listen(port, function () {
     console.log("Listening on port " + port);
